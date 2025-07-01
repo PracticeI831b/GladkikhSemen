5b7242ed58fdd52e548f030e8d9ea2da12606012
Teo-Mikki <88005553535oooaaa@gmail.com>
2025-07-01 16:13:03 +0300

    Project has been initialized

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("jvm")
+    id("org.jetbrains.compose")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+group = "GladkikhSemen"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "task6"
+            packageVersion = "1.0.0"
+        }
+    }
+}
